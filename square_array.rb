aaa6ec614b6d4b308cd8a76a9faece7d13a211f3
Julian <julianrosenthal@yahoo.com>
2020-11-09 17:04:48 -0700

    commit tree f9d90589388235ad56881635cc4d2735f40c2d63

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  array_squared = []
+  array.each do |ele|
+    array_squared.push(ele ** 2)
+  end
+  array_squared
 end
+
+##def square_array(array)
+##  array_squared = array.collect {|ele| ele ** 2}
+##  array_squared
+##end
